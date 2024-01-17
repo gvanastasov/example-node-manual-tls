1ccc96c3c83be6b010b2bed3d3fb2ab3f8b00333
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-01-17 11:55:03 +0100

    update docs on tls

--- src/tls-record-header.js
+++ src/tls-record-header.js
@@ -2,19 +2,57 @@ const { hexArray, hexStrategyMixin } = require('./utils');
 const { TLSVersion } = require('./tls-version');
 
 const ContentType = {
+    /**
+     * Used for exchanging cryptographic parameters and 
+     * keying material during the TLS handshake process. 
+     * Handshake messages include various types such as 
+     * ClientHello, ServerHello, Certificate, Finished, 
+     * etc. These messages play a crucial role in 
+     * establishing a secure connection between the client 
+     * and server.
+     */
     Handshake: 0x16,
+
+    /**
+     * Used to signal a change in the cryptographic parameters 
+     * for communication. This typically follows the handshake 
+     * messages and indicates that subsequent records will be 
+     * protected using the negotiated algorithms and keys.
+     */
     ChangeCipherSpec: 0x14,
+
+    /**
+     * Used for conveying error messages or warnings between the 
+     * client and server. Alert messages can indicate issues such 
+     * as unexpected closure of the connection, certificate problems, 
+     * or protocol-related errors. Alerts help in communicating 
+     * problems that may require immediate attention.
+     */
     Alert: 0x15,
+
+    /**
+     * Used for carrying the actual application data. Once the 
+     * handshake is complete and the secure channel is established, 
+     * application data, such as HTTP requests or responses in the 
+     * case of HTTPS, is transmitted using this content type.
+     */
     ApplicationData: 0x17,
 
     ...hexStrategyMixin
 };
 
 /**
- * @description Creates a TLS record header.
- * 1 byte - Handshake type;
- * 2 bytes - TLS version;
- * 2 bytes - placeholder for length;
+ * @description Creates a TLS record header. The content types 
+ * specified in the record header of a TLS message indicate the 
+ * type of content that follows in the payload.
+ * 
+ * Reserved 5 bytes
+ * 1 - Handshake type;
+ * 2 - TLS version;
+ * 2 - placeholder for length;
+ * 
+ * @example
+ * 0x16 0x03 0x01 0x00 0xa5
  * 
  * @param {number} contentType
  * @param {number} version
@@ -29,6 +67,11 @@ function createRecordHeader(contentType, version, length) {
     return header;
 }
 
+/**
+ * @description reads a buffer and converts to readable data
+ * @param {Buffer} buffer
+ * @returns an object representing record header
+ */
 function readRecordHeader(buffer) {
     const contentType = buffer.readUInt8(0);
     const version = buffer.readUInt16BE(1);
